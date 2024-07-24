13219722b72523ef57fd406f89cd10385829490f
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-24 23:55:31 +0300

    LoginViewController completed

--- BudgetManage/Scenes/Auth/Login/LoginViewController.swift
+++ BudgetManage/Scenes/Auth/Login/LoginViewController.swift
@@ -8,35 +8,73 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    //MARK: - Properties
-    
+    // MARK: - Properties
     private let loginView = LoginView()
+    private let viewModel = LoginViewModel()
+    private let userEmailKey = "userEmail"
     
-    //MARK: - Lifecycle
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupBindings()
+        populateUserEmail()
     }
-}
-
-// MARK: - setupUI & Actions
-extension LoginViewController{
-    private func setupUI(){
+    
+    // MARK: - UI Setup
+    private func setupUI() {
         self.navigationItem.hidesBackButton = true
         view.backgroundColor = .white
         view.addSubview(loginView)
         loginView.snp.makeConstraints { make in
             make.top.bottom.leading.trailing.equalToSuperview()
         }
-        self.action()
+        loginView.loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        loginView.toRegisterButton.addTarget(self, action: #selector(toRegisterButton), for: .touchUpInside)
     }
     
-    private func action(){
-        self.loginView.toRegisterButton.addTarget(self, action: #selector(toRegister), for: .touchUpInside)
+    // MARK: - Bindings Setup
+    private func setupBindings() {
+        viewModel.isUserLoggedIn = { [weak self] success, message in
+            guard let self = self else { return }
+            self.showAlert(message: message ?? "An error occurred.")
+        }
+        
+        viewModel.isInputValid = { [weak self] isValid in
+            self?.loginView.loginButton.isEnabled = isValid
+        }
+        
+        loginView.emailText.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        loginView.passwordText.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
     }
     
-    @objc
-    private func toRegister(){
+    // MARK: - Actions
+    @objc private func loginButtonTapped() {
+        viewModel.email = loginView.emailText.text ?? ""
+        viewModel.password = loginView.passwordText.text ?? ""
+        viewModel.loginUser()
+    }
+    
+    @objc private func textFieldDidChange() {
+        viewModel.email = loginView.emailText.text ?? ""
+        viewModel.password = loginView.passwordText.text ?? ""
+    }
+    
+    @objc private func toRegisterButton(){
         navigationController?.pushViewController(RegisterViewController(), animated: true)
     }
+    
+    // MARK: - Helpers
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func populateUserEmail() {
+        if let savedEmail = UserDefaults.standard.string(forKey: userEmailKey) {
+            loginView.emailText.text = savedEmail
+        }
+    }
 }
+
