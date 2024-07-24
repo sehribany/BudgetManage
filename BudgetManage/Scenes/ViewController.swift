ef0590ce77737d1e8c0a5c62ef466635b334bf94
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-24 15:39:57 +0300

    first configuration

---
+++ BudgetManage/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetManage
+//
+//  Created by Şehriban Yıldırım on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
