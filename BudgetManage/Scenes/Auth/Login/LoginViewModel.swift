df542f803d1ecedcf7087efe3027d2306e08728d
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-24 23:54:06 +0300

    LoginViewModel Completed

--- BudgetManage/Scenes/Auth/Login/LoginViewModel.swift
+++ BudgetManage/Scenes/Auth/Login/LoginViewModel.swift
@@ -6,7 +6,57 @@
 //
 
 import Foundation
+import Firebase
 
-class LoginViewModel{
+class LoginViewModel {
+    // MARK: - Properties
+    var email: String = "" {
+        didSet {
+            validateInput()
+        }
+    }
     
+    var password: String = "" {
+        didSet {
+            validateInput()
+        }
+    }
+    
+    // Callbacks for view to handle UI updates
+    var isUserLoggedIn: ((Bool, String?) -> Void)?
+    var isInputValid: ((Bool) -> Void)?
+    
+    // MARK: - Input Validation
+    private func validateInput() {
+        let isValid = !email.isEmpty && !password.isEmpty
+        isInputValid?(isValid)
+    }
+    
+    // MARK: - User Login
+    func loginUser() {
+        guard validateInputs() else {
+            isUserLoggedIn?(false, "Please fill in all fields.")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.isUserLoggedIn?(false, error.localizedDescription)
+                return
+            }
+            
+            guard (authResult?.user) != nil else {
+                self.isUserLoggedIn?(false, "User login failed.")
+                return
+            }
+            
+            self.isUserLoggedIn?(true, "User logged in successfully.")
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func validateInputs() -> Bool {
+        return !email.isEmpty && !password.isEmpty
+    }
 }
+
