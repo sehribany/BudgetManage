4c2b1f87ff7f83a2823476f839467d04bbfe2183
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-24 23:56:58 +0300

    registerviewcontroller created.

--- BudgetManage/Scenes/Auth/Register/RegisterViewController.swift
+++ BudgetManage/Scenes/Auth/Register/RegisterViewController.swift
@@ -8,36 +8,61 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
-    //MARK: - Properties
-    
+    // MARK: - Properties
     private let registerView = RegisterView()
-   
-    //MARK: - Lifecycle
+    private let viewModel = RegisterViewModel()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupBindings()
     }
-}
-
-// MARK: - setupUI & Actions
-extension RegisterViewController{
-    private func setupUI(){
+    
+    // MARK: - UI Setup
+    private func setupUI() {
         self.navigationItem.hidesBackButton = true
         view.backgroundColor = .white
         view.addSubview(registerView)
         registerView.snp.makeConstraints { make in
             make.top.bottom.leading.trailing.equalToSuperview()
         }
-        self.action()
+        registerView.registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        registerView.toLoginButton.addTarget(self, action: #selector(toLoginButton), for: .touchUpInside)
     }
     
-    private func action(){
-        self.registerView.toLoginButton.addTarget(self, action: #selector(toLogin), for: .touchUpInside)
+    // MARK: - Bindings Setup
+    private func setupBindings() {
+        viewModel.isUserRegistered = { [weak self] success, message in
+            guard let self = self else { return }
+            if success {
+                self.navigateToVerificationScreen()
+            } else {
+                self.showAlert(message: message ?? "An error occurred.")
+            }
+        }
     }
     
-    @objc
-    private func toLogin(){
+    // MARK: - Actions
+    @objc private func registerButtonTapped() {
+        viewModel.email = registerView.emailText.text ?? ""
+        viewModel.password = registerView.passwordText.text ?? ""
+        viewModel.registerUser()
+    }
+    
+    @objc private func toLoginButton(){
         navigationController?.pushViewController(LoginViewController(), animated: true)
+    }
+    
+    // MARK: - Helpers
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func navigateToVerificationScreen() {
+        let verificationVC = VerificationViewController()
+        navigationController?.pushViewController(verificationVC, animated: true)
     }
 }
