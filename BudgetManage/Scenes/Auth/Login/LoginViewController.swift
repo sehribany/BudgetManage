ee1e8c5c3ba2b530240b13c8ce00976682822524
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-24 18:11:42 +0300

    Added LoginView to LoginViewController

--- BudgetManage/Scenes/Auth/Login/LoginViewController.swift
+++ BudgetManage/Scenes/Auth/Login/LoginViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    //MARK: - Properties
+    
+    private let loginView = LoginView()
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+}
 
-        
+// MARK: - setupUI & Actions
+extension LoginViewController{
+    private func setupUI(){
+        self.navigationItem.hidesBackButton = true
+        view.backgroundColor = .white
+        view.addSubview(loginView)
+        loginView.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+        self.action()
     }
     
-
-
+    private func action(){
+        self.loginView.toRegisterButton.addTarget(self, action: #selector(toRegister), for: .touchUpInside)
+    }
+    
+    @objc
+    private func toRegister(){
+        navigationController?.pushViewController(RegisterViewController(), animated: true)
+    }
 }
