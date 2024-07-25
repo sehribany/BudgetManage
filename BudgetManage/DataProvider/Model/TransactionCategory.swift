6665a6b1f336f77aacdf8f92f72d6661630468b1
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-25 14:55:55 +0300

    TransactionCategory created.

---
+++ BudgetManage/DataProvider/Model/TransactionCategory.swift
@@ -0,0 +1,11 @@
+//
+//  TransactionCategory.swift
+//  BudgetManage
+//
+//  Created by Şehriban Yıldırım on 25.07.2024.
+//
+
+struct TransactionCategory: Codable {
+    let id: Int
+    let name: String
+}
